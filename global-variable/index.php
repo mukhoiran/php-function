e2e6e2f1f3f4d7f3d2fbb2bcc5d090526a011bf1
mukhoiran <mkhoir.anam@gmail.com>
2019-04-06 23:07:49 +0700

    login simple

--- global-variable/index.php
+++ global-variable/index.php
@@ -11,9 +11,24 @@
 //   echo $_POST['name'];
 // }
 
+
+$user = 'anam';
+$password = '123';
+
+if(isset($_POST['submit'])){
+  if($_POST['name'] == $user && $_POST['password'] == $password){
+    // echo "Login success";
+
+    //redirect
+    header('Location: profile.php?name='.$user);
+  }else{
+    echo "Login failed";
+  }
+}
+
 ?>
 
-<form class="" action="profile.php" method="get">
+<form class="" action="index.php" method="post">
     <input type="text" name="name" value="">
     <input type="password" name="password" value="">
     <input type="submit" name="submit" value="Submit">
