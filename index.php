075aaedfe95d3ce88ec573d4181c68010d4f2e0c
mukhoiran <mkhoir.anam@gmail.com>
2019-04-06 22:39:56 +0700

    implode and explode

--- index.php
+++ index.php
@@ -13,4 +13,20 @@
       Welcome to the PHP, one of programming language.
     </main>
 
+    <h2>Implode and Explode</h2>
+
+    <?php
+
+      //implode(separator,array) -> convert array to string
+      $jobs = ["programmer","designer","manager"];
+      echo implode(" ",$jobs);
+
+      echo "<hr>";
+
+      //explode(separator,string) -> convert string to array
+      $study = "html css javascript php";
+      print_r(explode(" ",$study));
+      // print_r(explode(" ",$study, 2)); --> limit array
+    ?>
+
 <?php include('footer.php'); ?>
