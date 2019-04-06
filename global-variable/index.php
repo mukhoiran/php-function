dcd5475d6e2f106f1bddf7b858ed6f98cb674e25
mukhoiran <mkhoir.anam@gmail.com>
2019-04-06 23:00:48 +0700

    get and post

---
+++ global-variable/index.php
@@ -0,0 +1,20 @@
+<?php
+//------------------------------
+//--------GET and POST----------
+//------------------------------
+
+// if(isset($_GET['submit'])){
+//   echo $_GET['name'];
+// }
+
+// if(isset($_POST['submit'])){
+//   echo $_POST['name'];
+// }
+
+?>
+
+<form class="" action="profile.php" method="get">
+    <input type="text" name="name" value="">
+    <input type="password" name="password" value="">
+    <input type="submit" name="submit" value="Submit">
+</form>
